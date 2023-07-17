a7c1f3e24a64f30803148b8e2f248842297b5209
Valeh Mammadli <valehlothbrok@gmail.com>
2023-07-17 16:18:27 +0400

    redux

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeFromCart } from '../../redux/cartSlice';
+import { FaTrash } from 'react-icons/fa';
+
 
 const Cart = () => {
-  const cartItems = useSelector((state) => state.cart);
+  const cartItems = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
+
+  // Get all unique categories from cart items
+  const categories = [...new Set(cartItems.map((item) => item.category))];
+
+  const handleRemoveFromCart = (itemId) => {
+    dispatch(removeFromCart(itemId));
+  };
+
 
   return (
-    <div>
-      {cartItems.map((item) => (
-        <div key={item.id}>
-          <div>{item.category}</div>
-          {item.title}
+    <div> <h1>Seçilən yeməklər</h1>
+      {categories.map((category) => (
+        <div key={category}>
+          <div>{category}</div>
+          {cartItems
+            .filter((item) => item.category === category)
+            .map((item) => (
+              <div key={item.id}>
+                <div>{item.title} <span><button onClick={() => handleRemoveFromCart(item.id)}>< FaTrash /></button></span></div>
+              </div>
+            ))}
         </div>
       ))}
+      <div>Total Price: {totalPrice}₼</div>
     </div>
   );
 };
--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -2,13 +2,38 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: [],
+  initialState: {
+    items: [],
+    totalPrice: 0,
+  },
   reducers: {
     addToCart: (state, action) => {
-      state.push(action.payload);
+      const { id, title, price, category } = action.payload;
+      const existingItemIndex = state.items.findIndex((item) => item.id === id);
+
+      if (existingItemIndex !== -1) {
+        state.totalPrice += price;
+      } else {
+        state.items.push({
+          id,
+          title,
+          price,
+          category,
+        });
+        state.totalPrice += price;
+      }
+    },
+    removeFromCart: (state, action) => {
+      const itemId = action.payload;
+      const existingItemIndex = state.items.findIndex((item) => item.id === itemId);
+
+      if (existingItemIndex !== -1) {
+        state.totalPrice -= state.items[existingItemIndex].price;
+        state.items.splice(existingItemIndex, 1);
+      }
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
